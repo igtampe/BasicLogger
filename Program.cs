b52e9a5df5d310ef6c743e2949a0f986cb40904b
Ignacio Tampe <Igtampe@gmail.com>
2022-03-22 22:53:35 -0400

    Fix namespace

--- Program.cs
+++ Program.cs
@@ -1,11 +1,11 @@
 ﻿using System;
 
-namespace BasicLogger {
+namespace Igtampe.BasicLogger {
     /// <summary>Test program</summary>
     internal static class Program {
         public static void Main() {
             Console.WriteLine("Hello, World!");
-            Igtampe.BasicLogger.Logger Logger = new Igtampe.BasicLogger.BasicLogger(Igtampe.BasicLogger.LogSeverity.DEBUG, "TestOutput.txt");
+            Logger Logger = new BasicLogger(LogSeverity.DEBUG, "TestOutput.txt");
 
             Logger.Debug("This is debug information");
             Logger.Info("This is Info information");
@@ -14,17 +14,17 @@ namespace BasicLogger {
             Logger.Fatal("This is another fatal exception");
 
             try { StackedError(2); } catch (Exception E) { Logger.Exception(E); }
-            try { StackedError(2); } catch (Exception E) { Logger.Exception(E, Igtampe.BasicLogger.LogSeverity.WARNING); }
+            try { StackedError(2); } catch (Exception E) { Logger.Exception(E, LogSeverity.WARNING); }
 
-            Igtampe.BasicLogger.Logger SubLogger = new Igtampe.BasicLogger.SubLogger("Sub", Logger);
+            Logger SubLogger = new SubLogger("Sub", Logger);
             SubLogger.Debug("This is debug information");
             SubLogger.Info("This is Info information");
             SubLogger.Warn("This is a Warning");
             SubLogger.Error("This is an Error");
             SubLogger.Fatal("This is another fatal exception");
 
-            try { StackedError(2); } catch (Exception E) { SubLogger.Exception(E, Igtampe.BasicLogger.LogSeverity.FATAL); }
-            try { StackedError(2); } catch (Exception E) { SubLogger.Exception(E, Igtampe.BasicLogger.LogSeverity.INFO); }
+            try { StackedError(2); } catch (Exception E) { SubLogger.Exception(E, LogSeverity.FATAL); }
+            try { StackedError(2); } catch (Exception E) { SubLogger.Exception(E, LogSeverity.INFO); }
 
             Console.WriteLine();
             Console.WriteLine("This concludes the demo");
